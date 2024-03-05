cb1dd27a1e19de5f88d4c715a72d57d06c0f9561
tmkvn <122834007+tmkvn@users.noreply.github.com>
2024-03-04 21:02:06 -0500

    Ejercicios 1-8 terminados

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function (num1, num2) {
+  return num1 + num2
 
 };
 
-const power = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2
 };
 
-const factorial = function() {
-	
+const sum = function (numbers) {
+  let result = 0
+  if (numbers.length !== 0) {
+    numbers.forEach(element => {
+      result += element
+    });
+  }
+  return result
+
 };
+
+const multiply = function (numbers) {
+  let result = 1
+  numbers.forEach(element => {
+    result *= element
+  });
+  return result
+};
+
+const power = function (base, exponent) {
+  return base ** exponent
+};
+
+const factorial = function (number) {
+  let result = 1
+  for (let i = 1; i <= number; i++) {
+    result *= i
+  }
+  return result
+}
 
 // Do not edit below this line
 module.exports = {
